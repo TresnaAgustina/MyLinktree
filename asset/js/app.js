c72839f71b0b8715ff46c3b95813355baac2705e
TresnaAgustina <tresnaa001@gmail.com>
2022-09-19 21:56:07 +0800

    add page loader

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,2 +1,11 @@
+
+// Parallax.js
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+// loader
+const loaderContainer = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+      loaderContainer.style.display = 'none';
+  }); 
