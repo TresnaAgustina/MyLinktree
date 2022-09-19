2a14f5b55a2218a2b1fbd7de0e9c235a206ae9b7
TresnaAgustina <tresnaa001@gmail.com>
2022-09-19 22:07:50 +0800

    fixing page loader

--- asset/js/app.js
+++ asset/js/app.js
@@ -4,8 +4,8 @@ var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 
 // loader
-const loaderContainer = document.querySelector('.loader');
+const loader = document.querySelector('.loader');
 
 window.addEventListener('load', () => {
-      loaderContainer.style.display = 'none';
+      loader.className += " hidden";
   }); 
